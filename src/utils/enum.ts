148f687eed9a6dec203b2fb27badbe4449ec404d
郑家豪 <beatboxzjh@163.com>
2025-12-15 01:22:11 +0800

    feat: 枚举更新

---
+++ src/utils/enum.ts
@@ -0,0 +1,8 @@
+export const levelEnum = [
+  { text: '管理员', id: 0 },
+  { text: '架构师/经理', id: 1 },
+  { text: '主管', id: 2 },
+  { text: '专责', id: 3 },
+  { text: '工程师', id: 4 },
+  { text: '实习生', id: 5 },
+];
